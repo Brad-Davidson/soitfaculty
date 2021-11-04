2dc76d6c1b35b6c902a5429607e55fb4a08f1fb6
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-11-04 13:45:24 -0400

    update

--- src/main/java/com/soit/soitfaculty/entity/Faculty.java
+++ src/main/java/com/soit/soitfaculty/entity/Faculty.java
@@ -1,23 +1,46 @@
-package com.soit.soitfaculty.model;
+package com.soit.soitfaculty.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="faculty")
 public class Faculty {
+
+
+	//Set the fields
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+
+	@Column(name="id")
 	private int id;
+	@Column(name="first_name")
 	private String firstName;
+	@Column(name="last_name")
 	private String lastName;
+	@Column(name="email")
 	private String email;
-	
+
+
+	//Set the Constructor
 	public Faculty() {
-		
+
 	}
 
 	public Faculty(int id, String firstName, String lastName, String email) {
-		super();
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
 
+
+	//Set the Getters and Setters
 	public int getId() {
 		return id;
 	}
@@ -54,6 +77,7 @@ public class Faculty {
 	public String toString() {
 		return "Faculty [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-	
-	
+
+
+
 }
---
+++ src/main/java/com/soit/soitfaculty/services/FacultyService.java
@@ -0,0 +1,16 @@
+package com.soit.soitfaculty.services;
+
+import java.util.List;
+
+import com.soit.soitfaculty.entity.Faculty;
+
+public interface FacultyService {
+
+	public List<Faculty> findAll();
+
+	public Faculty findById(int theId);
+
+	public void save(Faculty theFaculty);
+
+	public void deleteById(int theId);
+}
